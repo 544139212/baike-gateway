647131e6f204316eecbb2791f8323539e10c2d37
wangh <wangh@wolianw.com>
2017-10-27 20:50:57 +0800

    初始提交

---
+++ src/test/java/com/smx/BaikeGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.smx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaikeGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
